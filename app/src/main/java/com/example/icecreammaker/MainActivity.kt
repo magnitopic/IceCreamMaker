3ce07691d2cf29ef7e63dd48e1ee3fea10c2317c
magnitopic <magnitopic@gmail.com>
2023-01-09 12:09:04 +0100

    added scrollView and better code for checkbox

--- app/src/main/java/com/example/icecreammaker/MainActivity.kt
+++ app/src/main/java/com/example/icecreammaker/MainActivity.kt
@@ -3,34 +3,34 @@ package com.example.icecreammaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private val precioUnitario = 3
     private var cantidad = 1
-    private var cream = false
     private var name = ""
     private lateinit var textViewPrecio: TextView
     private lateinit var textViewCantidad: TextView
+    private lateinit var checkBoxCream: CheckBox
+    private lateinit var checkBoxChocolate: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textViewPrecio = findViewById(R.id.price)
         textViewCantidad = findViewById(R.id.amount)
+        checkBoxCream = findViewById(R.id.checkBoxCream)
+        checkBoxChocolate = findViewById(R.id.checkBoxChocolate)
     }
 
     fun onClickButtonOrder(vista: View) {
         displayPriceInformation()
     }
 
-    fun onCheckBoxChange(vista : View) {
-        cream = !cream
-    }
-
-    private fun displayPriceInformation() {
-        var info = ("Nombre: ${name}\n¿Incluir crema batida?: ${if (cream) "Sí" else "No"}\nCantidad: ${cantidad}\nTotal: ${calculatePrice()} €\n¡Gracias por su visita!")
+    private fun  displayPriceInformation() {
+        var info = ("Nombre: ${name}\n¿Incluir crema batida?: ${if (checkBoxCream.isChecked) "Sí" else "No"}\n¿Incluir chocolate?: ${if (checkBoxChocolate.isChecked) "Sí" else "No"}\nCantidad: ${cantidad}\nTotal: ${calculatePrice()} €\n¡Gracias por su visita!")
         textViewPrecio.text = info
     }
 
